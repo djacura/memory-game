3fc8826c1cd000f66570b27f152ef3d4f267f94d
Daryl <d.jacura@sky.com>
2020-06-16 16:45:54 +0000

    changed image names, and added javascript for matching cards

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,33 @@
 const cards = document.querySelectorAll('.memory-card');
 
-$('.memory-card').click(function() {
-  $(this).toggleClass('flip');
-});
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+function flipCard() {
+  $(this).toggleClass('flip');
+
+  if (!hasFlippedCard) {
+      // first click
+      hasFlippedCard = true;
+      firstCard = this;
+  } else {
+      // second click
+      hasFlippedCard = false;
+      secondCard = this;
+
+      // does card match?
+      if (firstCard.dataset.card === secondCard.dataset.card) {
+          // the cards match!
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+      } else {
+          // the cards do not match!
+          setTimeout(() => {
+          $(firstCard).removeClass('flip');
+          $(secondCard).removeClass('flip');
+          }, 1200);
+      }
+  }
+};
+
+cards.forEach(card => card.addEventListener('click', flipCard));
