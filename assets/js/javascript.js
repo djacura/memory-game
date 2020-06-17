fbac438243b39ae63ee0014bfaad2df3fb28203d
Daryl <d.jacura@sky.com>
2020-06-17 16:25:37 +0000

    added javascript to stop double clicking cards and added function for shuffle cards

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,33 +1,95 @@
 const cards = document.querySelectorAll('.memory-card');
 
+// setting up variables
+
 let hasFlippedCard = false;
+let lockBoard = false; // using this so that you can't click multiple cards at once before they check for match.
 let firstCard, secondCard;
 
 function flipCard() {
-  $(this).toggleClass('flip');
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    $(this).toggleClass('flip');
 
   if (!hasFlippedCard) {
+
       // first click
+
       hasFlippedCard = true;
       firstCard = this;
-  } else {
-      // second click
-      hasFlippedCard = false;
-      secondCard = this;
 
-      // does card match?
-      if (firstCard.dataset.card === secondCard.dataset.card) {
-          // the cards match!
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-      } else {
-          // the cards do not match!
-          setTimeout(() => {
-          $(firstCard).removeClass('flip');
-          $(secondCard).removeClass('flip');
-          }, 1200);
-      }
+      return;
   }
+
+    // second click
+
+    secondCard = this;
+
+    checkForMatch();
+  
 };
+
+// does the card match?
+
+function checkForMatch() {
+
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+
+    // the cards match!
+
+    disableCards();
+    
+    } else {
+
+    // the cards do not match!
+    
+    unflipCards();
+        
+    }
+};
+
+function disableCards() {
+
+    // removes the event listener so cannot click cards again after match.
+
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+      
+function unflipCards() {
+
+    lockBoard = true;
+
+    setTimeout(() => {
+
+        // removes the class 'flip' from the cards if they dont match.
+
+        $(firstCard).removeClass('flip');
+        $(secondCard).removeClass('flip');
+
+        resetBoard();
+
+        }, 1200);
+}  
+
+function resetBoard() {
+
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+
+} 
+
+(function shuffleCards() {
+    cards.forEach(card => {
+
+        let randomPos = Math.floor(Math.random() * 12);
+
+        card.style.order = randomPos;
+    });
+})();
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
